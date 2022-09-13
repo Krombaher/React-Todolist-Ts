4fecdc5fa2ee3896340a1e76a8941b33a15811f7
Vitalij Kazuro <vitalijkazuro939@gmail.com>
2022-09-13 18:17:17 +0300

    Map, Filter, UseState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
+
+    let initTasks = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Redux", isDone: false}
+    ]
+
+    const [tasks, setTasks] = useState(initTasks);
+
+    const removeTask = (id:number) => {
+        let filteredTasks = tasks.filter(t => t.id !== id)
+        setTasks(filteredTasks)
+    }
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+           <Todolist 
+           title={'What to Learn'} 
+           tasks={tasks}
+           removeTask={removeTask}
+           />
         </div>
     );
 }
